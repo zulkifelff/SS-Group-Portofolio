b9b0cf7f2003b6f8ba0fc1fc268e7cc1dbb41fa5
Muhammad Zulkifel <zulkifel@gmail.com>
2021-05-24 19:04:54 +0500

    Early Push for Setup

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include 'shared/header.php';?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+    crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Roboto+Slab:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>SS Group</title>
+  </head>
+  <body>
+    <?php include 'shared/header.php';?>
 
 
-<?php include 'shared/footer.php';?>
+    <?php include 'shared/footer.php';?>
+      </body>
+</html>
--- shared/header.php
+++ shared/header.php
@@ -1,3 +1,5 @@
 <header>
+<?php
 
+?>
 </header>
