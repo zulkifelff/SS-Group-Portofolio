bdd9c3126d3b56d51c24524ea0b7961d4dca310d
Muhammad Zulkifel <zulkifel@gmail.com>
2021-06-04 11:04:41 +0500

    Header Content Update

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -28,3 +28,36 @@ function closeLoader() {
 function openLoader() {
     jQuery('#loaderHolder').removeClass('d-none');
 }
+
+function menuBarToggle()
+{
+    // setTimeout(function () {
+    //     if(jQuery("#navbarHeader").hasClass("show"))
+    //     {
+    //         debugger;
+    //         jQuery("#navbarHeader").addClass('fixed-header-bar');
+    //         var heightOfNavbar=$('#navbarHeader').outerHeight();
+    //         jQuery(".navbar").css({"top": heightOfNavbar});
+    //         jQuery(".navbar").addClass('active');
+    //     }
+    //     else
+    //     {
+    //         jQuery("#navbarHeader").removeClass('fixed-header-bar');
+    //         jQuery(".navbar").css({"top": 0});
+    //         jQuery(".navbar").removeClass('active');
+    //     }
+    // }, 400);
+
+    jQuery("html, body").animate({ scrollTop: 0 }, 'fast');
+    return false;
+
+    console.log('Something is pressed');
+
+
+
+    // Upon Open
+
+
+
+    // Upon Close
+}
--- shared/header.php
+++ shared/header.php
@@ -8,7 +8,9 @@
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
                     <h4 style="opacity: 0" class="text-white">About</h4>
-                  <p class="text-white mt-15">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+                  <p class="text-white mt-15">
+                      SS Feed was established in year 2014 with a vision to follow the footsteps of the world leading giants of the poultry industry. Starting from the day one we are a quality conscious company, our commitment to humility, integrity, ethics, hard work and focused customer market, we have started developing our roots in the local feed market.  We, by providing continuous quality products, have earned respect amongst the customers, both individual and large scale.
+                  </p>
                 </div>
                 <div class="col-sm-4 offset-md-1 py-4">
                   <h4 class="text-white">S.S. Group</h4>
@@ -27,7 +29,12 @@
             <a href="../index.php" class="navbar-brand d-flex align-items-center">
                 <img src="assets/images/logo.png" alt="Logo">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="true" aria-label="Toggle navigation">
+            <button onclick="menuBarToggle()" class="navbar-toggler" type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarHeader"
+                    aria-controls="navbarHeader"
+                    aria-expanded="true"
+                    aria-label="Toggle navigation">
                 <span class="menu-text">Menu</span>
                 <span class="navbar-toggler-icon"></span>
             </button>
